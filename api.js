dcae6d203b195708555c2b47aa53a87fc8612e29
LuisFPR04 <luisfpera@gmail.com>
2023-03-25 11:44:46 -0300

    commit

--- api.js
+++ api.js
@@ -1,24 +1,21 @@
-
 function paises(){
     let xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
-        response(xhr.responseText);
-    }
-    };
+        initial_response(xhr.responseText);
+    }};
 
     xhr.open("GET", "https://restcountries.com/v3.1/all", true);
     xhr.send();
 }
 
-function response(res){
+function initial_response(res){
    console.log("llamada api resuelta");
 
    let tabla = document.getElementById("tabla");
 
-
-   let objeto = JSON.parse(res);
+   var objeto = JSON.parse(res);
    for (i = 0; i < objeto.length; i++){
    // console.log(`${objeto[i].name.common} aaaaaaaaaaaaaaaaaaaaaaaaaaaaa`)
     let nuevaFila = tabla.insertRow();
@@ -28,25 +25,34 @@ function response(res){
     celda1.textContent = objeto[i].altSpellings[0];
     celda2.textContent = objeto[i].name.common;
     celda3.textContent = objeto[i].region;
-
    }
-}
+};
 
-document.addEventListener('DOMContentLoaded', () => {
+/*document.addEventListener('DOMContentLoaded', () => {
     const btnDelTables = document.querySelector('#btnDelTables');
     btnDelTables.addEventListener('click', () => {
       const tabla = document.querySelector('#tabla');
       const filas = tabla.querySelectorAll('tr');
   
-      filas.forEach((fila) => {
-        const cellsToEliminate = fila.querySelectorAll('td');
-        cellsToEliminate.forEach((cell) => {
-          cell.remove();
+        filas.forEach((fila) => {
+            const cellsToEliminate = fila.querySelectorAll('td');
+            cellsToEliminate.forEach((cell) => {
+            cell.remove();
+            });
         });
-      });
     });
-  });
- 
+  }); */
+
+document.addEventListener('DOMContentLoaded', () => {
+    const miInput = document.getElementById("camp1");
+    miInput.addEventListener("input", (event) => {
+    const valor = event.target.value;
+    console.log("El valor actual del input es: ", valor);
+});
+});
+
+
+
   
   
   
