bde442b258e5a0298cbf9d666fd748e302feef6f
LuisFPR04 <luisfpera@gmail.com>
2023-03-26 12:23:56 -0300

    commit

--- api.js
+++ api.js
@@ -1,5 +1,7 @@
 /* API info */
 
+let filtro;
+
 function getCountryData() {
     return new Promise((resolve, reject) => {
       let xhr = new XMLHttpRequest();
@@ -18,8 +20,8 @@ function getCountryData() {
       xhr.send();
     });
 }
+
 document.addEventListener('DOMContentLoaded', function() {
-    
     getCountryData()
     .then(data => {
     initial_response(data);
@@ -32,20 +34,39 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function initial_response(data){
     let tabla = document.getElementById("tabla");
- 
+
+    filtro = document.getElementById("camp1").value;
+    var rowCount = tabla.rows.length;
+
+    if(rowCount>0){
+    for (let i = rowCount - 1; i >= 0; i--) {
+        tabla.deleteRow(i);
+    }}
+
     var objeto = JSON.parse(data);
     for (i = 0; i < objeto.length; i++){
-     let nuevaFila = tabla.insertRow();
-     let celda1 = nuevaFila.insertCell();
-     let celda2 = nuevaFila.insertCell();
-     let celda3 = nuevaFila.insertCell();
-     celda1.textContent = objeto[i].altSpellings[0];
-     celda2.textContent = objeto[i].name.common;
-     celda3.textContent = objeto[i].continents;
+
+      if(filtro.toLowerCase() === `${objeto[i].region.toLowerCase()}`){
+        let nuevaFila = tabla.insertRow();
+        let celda1 = nuevaFila.insertCell();
+        let celda2 = nuevaFila.insertCell();
+        let celda3 = nuevaFila.insertCell();
+        celda1.textContent = objeto[i].altSpellings[0];
+        celda2.textContent = objeto[i].name.common;
+        celda3.textContent = objeto[i].continents;
+        
+      } else if(filtro === null || filtro.trim()===""){
+        let nuevaFila = tabla.insertRow();
+        let celda1 = nuevaFila.insertCell();
+        let celda2 = nuevaFila.insertCell();
+        let celda3 = nuevaFila.insertCell();
+        celda1.textContent = objeto[i].altSpellings[0];
+        celda2.textContent = objeto[i].name.common;
+        celda3.textContent = objeto[i].continents;
+      }
     }
  }; 
 
-let regionText = '';
 function handleKeyDown(event) {
     if (event.key === "Enter") {
         regionText = event.target.value;
@@ -53,36 +74,7 @@ function handleKeyDown(event) {
     }
   } 
 
-    function prueba(data){
-    var objeto = JSON.parse(data)
-    var table = document.getElementById("tabla");
-    var rowCount = table.rows.length;
-    for (let i = rowCount - 1; i >= 0; i--) {
-        table.deleteRow(i);
-    }
-    for (i = 0; i < objeto.length; i++){
-        if (objeto[i].region === `${regionText}`){
-          let nuevaFila = tabla.insertRow();
-          let celda1 = nuevaFila.insertCell();
-          let celda2 = nuevaFila.insertCell();
-          let celda3 = nuevaFila.insertCell();
-          celda1.textContent = objeto[i].altSpellings[0];
-          celda2.textContent = objeto[i].name.common;
-          celda3.textContent = objeto[i].continents;
-        }
-  }
-}
-
 /* Asincronus Functions, button functions*/
-
-function EjecutaPrueba() {
-    getCountryData()
-        .then(data => {
-            prueba(data);
-        })
-        .catch(error => console.error(error));
-};
-
 function loadData() {
     getCountryData()
         .then(data => {
